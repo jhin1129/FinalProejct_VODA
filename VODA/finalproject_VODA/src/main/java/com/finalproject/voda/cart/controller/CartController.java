90bea1ec7ec52b920e0a40d12847da6e483f18b3
DESKTOP-TOH726Q\rh <foguddl31@gmail.com>
2022-10-08 18:38:32 +0900

    kangraehyoung / 장바구니 컨트롤러

--- VODA/finalproject_VODA/src/main/java/com/finalproject/voda/cart/controller/CartController.java
+++ VODA/finalproject_VODA/src/main/java/com/finalproject/voda/cart/controller/CartController.java
@@ -12,6 +12,7 @@ import org.springframework.web.servlet.ModelAndView;
 import com.finalproject.voda.cart.model.service.CartService;
 import com.finalproject.voda.cart.model.vo.Cart;
 import com.finalproject.voda.common.util.PageInfo;
+import com.finalproject.voda.product.model.vo.Product;
 
 @Controller
 @RequestMapping("/product")
@@ -34,4 +35,25 @@ public class CartController {
 		return model;
 		
 	}
+	
+//	@GetMapping("/cart_insert")
+//	public ModelAndView ProductDelete(
+//						ModelAndView model,
+//						@RequestParam int pno) {
+//		int result = 0;
+//		Cart cart = null;
+//		
+//		result = service.insertCart(pno);
+//		
+//		if(result > 0) {
+//			model.addObject("msg", "게시글이 정상적으로 삭제되었습니다.");
+//			model.addObject("location", "/product/product_all_list");
+//		} else {
+//			model.addObject("msg", "게시글 삭제를 실패하였습니다.");
+//			model.addObject("location", "/product/product_detail?pno=" + pno);
+//		}
+//		model.setViewName("common/msg");
+//		
+//		return model;
+//	}
 }
