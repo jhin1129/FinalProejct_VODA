813c78e7cddeeadebd1ef3e6ef9abefed4d308d2
qnflqnfl1004 <qnflqnfl1004@naver.com>
2022-10-19 01:35:03 +0900

    20221019 랭킹페이지 산으로 가는중..

--- VODA/finalproject_VODA/src/main/java/com/finalproject/voda/ranking/controller/RankingController.java
+++ VODA/finalproject_VODA/src/main/java/com/finalproject/voda/ranking/controller/RankingController.java
@@ -1,11 +1,14 @@
 package com.finalproject.voda.ranking.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.finalproject.voda.contents.model.vo.Contents;
 import com.finalproject.voda.ranking.model.service.RankingService;
 
 
@@ -17,10 +20,13 @@ public class RankingController {
 	
 	@GetMapping("/ranking")
 	public ModelAndView ranking(ModelAndView model) {
-		int count = 0;
 		
-		count = service.getCount();
+		List<Contents> ranking = null;
 		
+		ranking = service.getRanking("영화");
+		
+		
+		model.addObject("ranking", ranking);
 		model.setViewName("ranking/ranking");
 		
 		return model;
